0e07b88bc07e33c9ae5859e72f5f00c754237ad9
brendtmcfeeley <brendtmcfeeley@gmail.com>
2017-04-06 23:50:30 -1000

    Addition of very first mockup

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -8,24 +8,31 @@ FlowRouter.route('/', {
   },
 });
 
-FlowRouter.route('/list', {
-  name: 'List_Stuff_Page',
+FlowRouter.route('/view_all', {
+  name: 'View_All_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'View_All_Page' });
   },
 });
 
-FlowRouter.route('/add', {
-  name: 'Add_Stuff_Page',
+FlowRouter.route('/add_request', {
+  name: 'Add_Request_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Add_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Add_Request_Page' });
   },
 });
 
-FlowRouter.route('/stuff/:_id', {
-  name: 'Edit_Stuff_Page',
+FlowRouter.route('/menu', {
+  name: 'Browse_Menu_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Edit_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Browse_Menu_Page' });
+  },
+});
+
+FlowRouter.route('/about', {
+  name: 'About_Us_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'About_Us_Page' });
   },
 });
 
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,10 +1,13 @@
-import './add-stuff-page.html';
-import './add-stuff-page.js';
-
-import './edit-stuff-page.html';
-import './edit-stuff-page.js';
-
 import './home-page.html';
 
-import './list-stuff-page.html';
-import './list-stuff-page.js';
+import './view-all-page.html';
+import './view-all-page.js';
+
+import './add-request-page.html';
+import './add-request-page.js';
+
+import './browse-menu-page.html';
+import './browse-menu-page.js';
+
+import './about-us-page.html';
+import './about-us-page.js';
