efcc851c39d00bebc361ce66baf421dfb904d6b5
r-robles <rfrobles4@gmail.com>
2017-04-27 18:58:47 -1000

    Hide request upon clicking clicking fulfill

--- app/imports/ui/pages/fulfill-request.js
+++ app/imports/ui/pages/fulfill-request.js
@@ -3,6 +3,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Requests, RequestsSchema } from '../../api/requests/requests.js';
 
 Template.Fulfill_Request_Page.onCreated(function onCreated() {
+  this.subscribe('Requests');
   this.context = RequestsSchema.namedContext('Fulfill_Request_Page');
 });
 
@@ -14,11 +15,10 @@ Template.Fulfill_Request_Page.helpers({
     const requestData = Requests.findOne({ _id: FlowRouter.getParam('_id') });
     return requestData && requestData[fieldName];
   },
-});
-
-Template.Fulfill_Request_Page.events({
-  'submit .fulfill-request'(event, instance) {
-    event.preventDefault();
+  /**
+   * Removes the request from the View Requests page.
+   */
+  removeRequest() {
     const requestData = Requests.findOne({ _id: FlowRouter.getParam('_id') });
     const first = requestData.first;
     const last = requestData.last;
@@ -29,6 +29,28 @@ Template.Fulfill_Request_Page.events({
     const fulfilled = true;
 
     const updatedRequestData = { first, last, dorm, room, phone, requestedFoods, fulfilled };
+    Requests.update(requestData._id, { $set: updatedRequestData });
+  }
+});
+
+Template.Fulfill_Request_Page.events({
+  'submit .fulfill-request'(event) {
+    event.preventDefault();
+    FlowRouter.go('View_All_Page');
+  },
+  'submit .cancel-request'(event, instance) {
+    // On cancel, the request will reappear again
+    event.preventDefault();
+    const requestData = Requests.findOne({ _id: FlowRouter.getParam('_id') });
+    const first = requestData.first;
+    const last = requestData.last;
+    const dorm = requestData.dorm;
+    const room = requestData.room;
+    const phone = requestData.phone;
+    const requestedFoods = requestData.foodsRequested;
+    const fulfilled = false;
+
+    const updatedRequestData = { first, last, dorm, room, phone, requestedFoods, fulfilled };
 
     instance.context.resetValidation();
     RequestsSchema.clean(updatedRequestData);
@@ -36,9 +58,5 @@ Template.Fulfill_Request_Page.events({
       Requests.update(requestData._id, { $set: updatedRequestData });
       FlowRouter.go('View_All_Page');
     }
-  },
-  'submit .cancel-request'(event) {
-    event.preventDefault();
-    FlowRouter.go('View_All_Page');
   },
 });
